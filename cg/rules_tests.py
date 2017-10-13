e71972815feeb2dba494f10eba495707ca667da9
meawoppl <meawoppl@gmail.com>
2017-10-13 14:39:34 -0700

    PEP8

--- cg/rules_tests.py
+++ cg/rules_tests.py
@@ -7,22 +7,22 @@ from cg import rules
 def test_nullPlayLegality():
     """Make sure null play is allowed"""
     for _n in range(50):
-        stateVector = random(50, 0.5)
+        state_vector = random(50, 0.5)
 
         # Null play is _always_ legal!
         pv = Vector(np.zeros(50))
-        assert rules.isPlayLegal(stateVector, pv), "Null play comes back as illegal :("
+        assert rules.isPlayLegal(state_vector, pv), "Null play comes back as illegal :("
 
 
 def test_singlePlay():
     """Make sure each player will play"""
-    playSize = 50
-    for n in range(playSize):
-        s = np.zeros(playSize)
+    play_size = 50
+    for n in range(play_size):
+        s = np.zeros(play_size)
         s[n] = 1
         sv = Vector(s)
 
-        p = np.zeros(playSize)
+        p = np.zeros(play_size)
         p[0] = 1
         pv = Vector(p)
 
@@ -32,42 +32,42 @@ def test_singlePlay():
 def test_tiny_game_complete():
     """Sanity check all the legal moves of a small game"""
 
-    beginState = Vector((0, 1))
-    noMove = Vector((0, 0))
+    begin_state = Vector((0, 1))
+    no_move = Vector((0, 0))
 
     # No one moves
-    nextState = rules.propagateState(beginState, noMove, noMove)
-    assert beginState == nextState
+    next_state = rules.propagateState(begin_state, no_move, no_move)
+    assert begin_state == next_state
 
-    # Only player 1 Moves (offence) -> win for p1
-    nextState = rules.propagateState(beginState, Vector((1, 0)), noMove)
-    assert nextState == Vector((1, 1))
+    # Only player 1 Moves (offense) -> win for p1
+    next_state = rules.propagateState(begin_state, Vector((1, 0)), no_move)
+    assert next_state == Vector((1, 1))
 
     # Only player 1 Moved (defense) -> NC
-    nextState = rules.propagateState(beginState, Vector((0, 1)), noMove)
-    assert nextState == beginState
+    next_state = rules.propagateState(begin_state, Vector((0, 1)), no_move)
+    assert next_state == begin_state
 
-    # Only player 2 moves (offence) -> win for p2
-    nextState = rules.propagateState(beginState, noMove, Vector((0, 1)))
-    assert nextState == Vector((0, 0))
+    # Only player 2 moves (offense) -> win for p2
+    next_state = rules.propagateState(begin_state, no_move, Vector((0, 1)))
+    assert next_state == Vector((0, 0))
 
-    # Only player 2 moved (defence) -> NC
-    nextState = rules.propagateState(beginState, noMove, Vector((1, 0)))
-    assert nextState == beginState
+    # Only player 2 moved (defense) -> NC
+    next_state = rules.propagateState(begin_state, no_move, Vector((1, 0)))
+    assert next_state == begin_state
 
     # p1 and p2 both make complimentary moves (off/def) -> NC
     m1 = Vector((0, 1))
-    nextState = rules.propagateState(beginState, m1, m1)
-    assert nextState == beginState
+    next_state = rules.propagateState(begin_state, m1, m1)
+    assert next_state == begin_state
 
     m2 = Vector((1, 0))
-    nextState = rules.propagateState(beginState, m2, m2)
-    assert nextState == beginState
+    next_state = rules.propagateState(begin_state, m2, m2)
+    assert next_state == begin_state
 
     # Both attack (all bits flipped!)
-    nextState = rules.propagateState(beginState, Vector((1, 0)), Vector((0, 1)))
-    assert nextState == Vector((1, 0))
+    next_state = rules.propagateState(begin_state, Vector((1, 0)), Vector((0, 1)))
+    assert next_state == Vector((1, 0))
 
     # Both defend (no bits flipped)
-    nextState = rules.propagateState(beginState, Vector((0, 1)), Vector((1, 0)))
-    assert nextState == Vector((0, 1))
+    next_state = rules.propagateState(begin_state, Vector((0, 1)), Vector((1, 0)))
+    assert next_state == Vector((0, 1))
